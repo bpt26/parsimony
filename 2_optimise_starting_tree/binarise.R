f8256b6fd2d35c5d72760f948bd93db92ef26256
roblanf <rob.lanfear@gmail.com>
2021-05-04 09:25:57 +1000

    unroot tree while binarising...

--- 2_optimise_starting_tree/binarise.R
+++ 2_optimise_starting_tree/binarise.R
@@ -11,4 +11,6 @@ input.tree$edge.length = ifelse(is.na(input.tree$edge.length), 0, input.tree$edg
 
 output.tree = multi2di(input.tree)
 
+output.tree = unroot(output.tree)
+
 write.tree(output.tree, output.tree.filename)
