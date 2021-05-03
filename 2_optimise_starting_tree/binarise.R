d3d8e58125cd6f841632b343ab709a40e16d0a47
roblanf <rob.lanfear@gmail.com>
2021-05-03 14:37:25 +1000

    make an usher tree binary
    using ape

---
+++ 2_optimise_starting_tree/binarise.R
@@ -0,0 +1,14 @@
+args = commandArgs(trailingOnly=TRUE)
+
+library(ape)
+
+input.tree.filename = args[1]
+output.tree.filename = args[2]
+
+input.tree = read.tree(input.tree.filename)
+
+input.tree$edge.length = ifelse(is.na(tree$edge.length), 0, tree$edge.length)
+
+output.tree = multi2di(input.tree)
+
+write.tree(output.tree, output.tree.filename)
