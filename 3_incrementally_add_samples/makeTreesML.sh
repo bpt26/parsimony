53f22558b90e1766a17ece9492d4132ea79fd8a2
Bryan Thornlow <bthornlo@ucsc.edu>
2021-05-06 11:58:08 -0700

    Add files via upload

--- 3_incrementally_add_samples/makeTreesML.sh
+++ 3_incrementally_add_samples/makeTreesML.sh
@@ -5,5 +5,6 @@ for i in {2..75}
   do 
   	h=$((i-1));
   	usher -i ${h}_samples.opt.pb -v ../BATCH_VCFs/${i}_samples.vcf -o ${i}_samples.0.pb
+  	matUtils extract -i ${i}_samples.0.pb -R -t ${i}_samples.0.nwk
   	bash logOptimizeML.sh ${i}_samples
 done;
