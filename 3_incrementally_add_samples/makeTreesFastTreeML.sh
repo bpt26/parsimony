bfb26848fb4d2c9fc25a1e57ac447623584e0c00
Bryan Thornlow <bthornlo@ucsc.edu>
2021-05-12 18:19:54 -0700

    Add files via upload

--- 3_incrementally_add_samples/makeTreesFastTreeML.sh
+++ 3_incrementally_add_samples/makeTreesFastTreeML.sh
@@ -7,5 +7,5 @@ for i in {2..75}
   	h=$((i-1));
   	usher -i ${h}_samples.opt.pb -v ../BATCH_VCFs/${i}_samples.vcf -o ${i}_samples.0.pb
   	matUtils extract -i ${i}_samples.0.pb -R -t ${i}_samples.0.nwk
-  	bash logOptimizeML.sh ${i}_samples
+  	bash logOptimizeFastTreeML.sh ${i}_samples
 done;
