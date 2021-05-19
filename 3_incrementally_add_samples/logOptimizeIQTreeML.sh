f4e1dced77e33b619e99acfb44b0df827f2ecdd7
Bryan Thornlow <bthornlo@ucsc.edu>
2021-05-18 19:45:31 -0700

    upload tree-making scripts

---
+++ 3_incrementally_add_samples/logOptimizeIQTreeML.sh
@@ -0,0 +1,5 @@
+cat temp.iqtree.fasta ../BATCH_FASTAS/KEEP/$1.fasta > use.iqtree.fasta
+iqtree2 -s use.iqtree.fasta -t $1.ml.iqtree.0.nwk -n 0 -m GTR+G --suppress-list-of-sequences -nt 8 -blmin 0.000000001 -pre $1.iqtree2
+mv use.iqtree.fasta temp.iqtree.fasta
+mv $1.iqtree2.treefile $1.ml.iqtree.opt.nwk
+
