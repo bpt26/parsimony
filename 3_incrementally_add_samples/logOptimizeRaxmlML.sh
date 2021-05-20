0771f02b50913e785a07e60a79b20fe6e85952e4
Bryan Thornlow <bthornlo@ucsc.edu>
2021-05-20 13:33:32 -0700

    Add files via upload

--- 3_incrementally_add_samples/logOptimizeRaxmlML.sh
+++ 3_incrementally_add_samples/logOptimizeRaxmlML.sh
@@ -1,3 +1,5 @@
 cat temp.raxml.fasta ../BATCH_FASTAS/KEEP/$1.fasta > use.raxml.fasta
 raxml-ng --model GTR+G --tree $1.ml.raxml.0.nwk --msa use.raxml.fasta --blmin 0.000000001  --threads  8
 mv use.raxml.fasta temp.raxml.fasta
+mv use.raxml.fasta.raxml.log $1.ml.raxml.log
+rm use.raxml.fasta.raxml*
