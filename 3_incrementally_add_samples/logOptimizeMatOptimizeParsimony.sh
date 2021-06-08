0b1feb583563ffec2fe362505c313feb1c0ed7e0
Bryan Thornlow <bthornlo@ucsc.edu>
2021-06-08 15:44:15 -0700

    add thread argument

--- 3_incrementally_add_samples/logOptimizeMatOptimizeParsimony.sh
+++ 3_incrementally_add_samples/logOptimizeMatOptimizeParsimony.sh
@@ -5,8 +5,8 @@ while [ $curr -lt $prev ]
 do
   prev=$curr;
   j=$((i+1))
-  matOptimize -i $1.$i.pb -o $1.$j.pb -s 259200 2>&1 | tee $1.iter_$j.log 
-  curr=$(matUtils summary -i $1.$j.pb | grep "Parsimony" | awk '{print $4}');
+  matOptimize -T 32 -i $1.$i.pb -o $1.$j.pb -s 259200 2>&1 | tee $1.iter_$j.log 
+  curr=$(matUtils -T 32 summary -i $1.$j.pb | grep "Parsimony" | awk '{print $4}');
   i=$j
 done;
 mv $1.$j.pb $1.opt.pb 
--- 3_incrementally_add_samples/makeTreesMatOptimizeParsimony.sh
+++ 3_incrementally_add_samples/makeTreesMatOptimizeParsimony.sh
@@ -1,9 +1,9 @@
-usher -t empty.nwk -v ../BATCH_VCFs/1_samples.vcf -o 1_samples.0.pb
+usher -t empty.nwk -v ../BATCH_VCFs/1_samples.vcf -o 1_samples.0.pb -T 32
 bash logOptimizeMatOptimizeParsimony.sh 1_samples
 i=2;
 for i in {2..75}
   do 
   	h=$((i-1));
-  	usher -i ${h}_samples.opt.pb -v ../BATCH_VCFs/${i}_samples.vcf -o ${i}_samples.0.pb
+  	usher -i ${h}_samples.opt.pb -v ../BATCH_VCFs/${i}_samples.vcf -o ${i}_samples.0.pb -T 32
   	bash logOptimizeMatOptimizeParsimony.sh ${i}_samples
 done;
