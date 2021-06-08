158cea402bdd0865da2675e08818e2bd70f0363e
Bryan Thornlow <bthornlo@ucsc.edu>
2021-06-08 15:47:41 -0700

    Add files via upload

--- 3_incrementally_add_samples/logOptimizeMatOptimizeParsimony.sh
+++ 3_incrementally_add_samples/logOptimizeMatOptimizeParsimony.sh
@@ -1,4 +1,4 @@
-curr=$(matUtils summary -T 32 -i $1.0.pb | grep "Parsimony" | awk '{print $4}');
+curr=$(matUtils summary -i $1.0.pb -T 32 | grep "Parsimony" | awk '{print $4}');
 prev=$((curr + 1));
 i=0;
 while [ $curr -lt $prev ]
@@ -6,7 +6,7 @@ do
   prev=$curr;
   j=$((i+1))
   matOptimize -T 32 -i $1.$i.pb -o $1.$j.pb -s 259200 2>&1 | tee $1.iter_$j.log 
-  curr=$(matUtils -T 32 summary -i $1.$j.pb | grep "Parsimony" | awk '{print $4}');
+  curr=$(matUtils summary -i $1.$j.pb -T 32 | grep "Parsimony" | awk '{print $4}');
   i=$j
 done;
 mv $1.$j.pb $1.opt.pb 
