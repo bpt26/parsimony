f5374bbaddc46a6c4fe4292f639c745946f9cef5
Bryan Thornlow <bthornlo@ucsc.edu>
2021-08-05 14:40:03 -0700

    update 4

--- 4_comparisons/README.md
+++ 4_comparisons/README.md
@@ -1,12 +1,20 @@
 # comparisons
 
-In this directory, we compare the trees produced in previous steps by various tree congruence metrics, such as Robinson-Foulds distance.
+In this directory, we compare the trees produced in previous steps by various tree congruence metrics, such as Robinson-Foulds distance. We have a few sets of trees that we can compare against each other:
+
+* matOptimized batch trees (1-50) from subdirectory 3
+* fasttree optimized batch trees (1-22) from subdirectory 3
+* IQ-TREE 2 optimized batch trees (1-13) from subdirectory 3
+* unoptimized matUtils trees (1-50) created in this directory by pruning down our starting tree (Section 4.1.1)
+* unoptimized UShER trees (1-50) created in this directory using only a .VCF (Section 4.1.2)
+
+^ We will compare each of these through Robinson-Foulds scores in order to determine their congruence to each other.
+
+Additionally, we have various larger trees from subdirectory 2 that we have already compared by parsimony score, and are in the process of simulating data based on the most optimal tree (after_usher_optimized_fasttree_iter6.tree.xz). This will enable comparisons to "ground truth" data, but remains in progress.
 
 ## 4.1.1: matOptimized batch trees vs pruned batch trees
 
 ```
-mkdir SAMPLES_LISTS/
-bash getSamplesLists.sh
 mkdir MAIN_PB_PRUNED/
 bash prunePb.sh
 bash convertNwk.sh
@@ -16,5 +24,14 @@ python plotMatOptVsPruned.py
 tar cfJ matopt_vs_pruned.tar.xz MATOPT_TREECMP_RESULTS/
 ```
 ## 4.1.2: matOptimized batch trees vs from-scratch batch trees
+
+```
+mkdir SAMPLES_LISTS/
+bash getSamplesLists.sh
+mkdir CUMULATIVE_VCFs/
+bash getCumulativeVCFs.sh
+mkdir FROM_SCRATCH_TREES/
+bash makeFromScratchTrees.sh
+```
 
 
---
+++ 4_comparisons/makeFromScratchTrees.sh
@@ -0,0 +1,100 @@
+usher -v CUMULATIVE_VCFs/1_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/1_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/1_samples.pb -t FROM_SCRATCH_TREES/1_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/2_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/2_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/2_samples.pb -t FROM_SCRATCH_TREES/2_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/3_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/3_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/3_samples.pb -t FROM_SCRATCH_TREES/3_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/4_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/4_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/4_samples.pb -t FROM_SCRATCH_TREES/4_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/5_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/5_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/5_samples.pb -t FROM_SCRATCH_TREES/5_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/6_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/6_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/6_samples.pb -t FROM_SCRATCH_TREES/6_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/7_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/7_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/7_samples.pb -t FROM_SCRATCH_TREES/7_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/8_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/8_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/8_samples.pb -t FROM_SCRATCH_TREES/8_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/9_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/9_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/9_samples.pb -t FROM_SCRATCH_TREES/9_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/10_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/10_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/10_samples.pb -t FROM_SCRATCH_TREES/10_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/11_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/11_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/11_samples.pb -t FROM_SCRATCH_TREES/11_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/12_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/12_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/12_samples.pb -t FROM_SCRATCH_TREES/12_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/13_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/13_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/13_samples.pb -t FROM_SCRATCH_TREES/13_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/14_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/14_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/14_samples.pb -t FROM_SCRATCH_TREES/14_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/15_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/15_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/15_samples.pb -t FROM_SCRATCH_TREES/15_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/16_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/16_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/16_samples.pb -t FROM_SCRATCH_TREES/16_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/17_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/17_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/17_samples.pb -t FROM_SCRATCH_TREES/17_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/18_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/18_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/18_samples.pb -t FROM_SCRATCH_TREES/18_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/19_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/19_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/19_samples.pb -t FROM_SCRATCH_TREES/19_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/20_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/20_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/20_samples.pb -t FROM_SCRATCH_TREES/20_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/21_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/21_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/21_samples.pb -t FROM_SCRATCH_TREES/21_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/22_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/22_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/22_samples.pb -t FROM_SCRATCH_TREES/22_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/23_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/23_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/23_samples.pb -t FROM_SCRATCH_TREES/23_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/24_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/24_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/24_samples.pb -t FROM_SCRATCH_TREES/24_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/25_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/25_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/25_samples.pb -t FROM_SCRATCH_TREES/25_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/26_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/26_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/26_samples.pb -t FROM_SCRATCH_TREES/26_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/27_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/27_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/27_samples.pb -t FROM_SCRATCH_TREES/27_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/28_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/28_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/28_samples.pb -t FROM_SCRATCH_TREES/28_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/29_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/29_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/29_samples.pb -t FROM_SCRATCH_TREES/29_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/30_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/30_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/30_samples.pb -t FROM_SCRATCH_TREES/30_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/31_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/31_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/31_samples.pb -t FROM_SCRATCH_TREES/31_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/32_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/32_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/32_samples.pb -t FROM_SCRATCH_TREES/32_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/33_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/33_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/33_samples.pb -t FROM_SCRATCH_TREES/33_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/34_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/34_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/34_samples.pb -t FROM_SCRATCH_TREES/34_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/35_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/35_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/35_samples.pb -t FROM_SCRATCH_TREES/35_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/36_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/36_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/36_samples.pb -t FROM_SCRATCH_TREES/36_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/37_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/37_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/37_samples.pb -t FROM_SCRATCH_TREES/37_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/38_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/38_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/38_samples.pb -t FROM_SCRATCH_TREES/38_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/39_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/39_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/39_samples.pb -t FROM_SCRATCH_TREES/39_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/40_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/40_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/40_samples.pb -t FROM_SCRATCH_TREES/40_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/41_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/41_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/41_samples.pb -t FROM_SCRATCH_TREES/41_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/42_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/42_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/42_samples.pb -t FROM_SCRATCH_TREES/42_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/43_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/43_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/43_samples.pb -t FROM_SCRATCH_TREES/43_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/44_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/44_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/44_samples.pb -t FROM_SCRATCH_TREES/44_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/45_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/45_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/45_samples.pb -t FROM_SCRATCH_TREES/45_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/46_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/46_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/46_samples.pb -t FROM_SCRATCH_TREES/46_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/47_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/47_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/47_samples.pb -t FROM_SCRATCH_TREES/47_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/48_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/48_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/48_samples.pb -t FROM_SCRATCH_TREES/48_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/49_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/49_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/49_samples.pb -t FROM_SCRATCH_TREES/49_samples.nwk -T 10
+usher -v CUMULATIVE_VCFs/50_samples.vcf -t empty.nwk -o FROM_SCRATCH_TREES/50_samples.pb -T 10
+matUtils extract -i FROM_SCRATCH_TREES/50_samples.pb -t FROM_SCRATCH_TREES/50_samples.nwk -T 10
