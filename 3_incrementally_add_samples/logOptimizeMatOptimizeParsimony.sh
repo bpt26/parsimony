c07fb07c9ab3aa9f4c17ef5e51b24bc7fcfe274e
Bryan Thornlow <bthornlo@ucsc.edu>
2021-08-11 15:07:20 -0700

    Update logOptimizeMatOptimizeParsimony.sh

--- 3_incrementally_add_samples/logOptimizeMatOptimizeParsimony.sh
+++ 3_incrementally_add_samples/logOptimizeMatOptimizeParsimony.sh
@@ -5,8 +5,8 @@ while [ $curr -lt $prev ]
 do
   prev=$curr;
   j=$((i+1))
-  /usr/bin/time -o $1.$i.matOptimize.time -f "%E %M" matOptimize -T 32 -i $1.$i.pb -o $1.$j.pb -s 259200
+  /usr/bin/time -o $1.$i.matOptimize.time -f "%E %M" matOptimize -T 32 -i $1.$i.pb -v CUMULATIVE_VCFs/$1.vcf.gz -o $1.$j.pb -s 259200
   curr=$(matUtils summary -i $1.$j.pb -T 32 | grep "Parsimony" | awk '{print $4}');
   i=$j
 done;
-mv $1.$j.pb $1.opt.pb 
+mv $1.$j.pb $1.opt.pb
