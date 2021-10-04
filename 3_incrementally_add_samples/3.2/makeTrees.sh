9650ddfc310075b05b17440337840c11b879fae9
Bryan Thornlow <bthornlo@ucsc.edu>
2021-10-04 14:16:02 -0700

    Create makeTrees.sh

---
+++ 3_incrementally_add_samples/3.2/makeTrees.sh
@@ -0,0 +1,5 @@
+for i in {1..50}
+  do
+    rm *ckp*
+    /usr/bin/time -o ${i}.denovo.iq.time -f "%E %M" iqtree2 -s /mnt/PARSIMONY_REAL_DATA/AGGREGATE_FASTAS/${i}.fasta -n 0 -m GTR+G --suppress-list-of-sequences -nt 15 -blmin 0.000000001 -pre ${i}_samples.iqtree2.denovo
+ done;
