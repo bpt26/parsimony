bc593db26885bdc696eb5d0b333801cbfbf4f5b3
Bryan Thornlow <bthornlo@ucsc.edu>
2021-11-20 13:59:40 -0500

    Update and rename logOptimizeFastTreeML.sh to logOptimizeFastTree.sh

--- 3_incrementally_add_samples/3.3/logOptimizeFastTreeML.sh
+++
@@ -1,3 +0,0 @@
-export OMP_NUM_THREADS=15
-/usr/bin/time -o $1.ft.time -f "%E %M" FastTreeMP -nt -sprlength 1000 -nni 0 -spr 2 -log $1.fasttree.log -nosupport -intree $1_samples.ml.fasttree.0.nwk /mnt/PARSIMONY_REAL_DATA/AGGREGATE_FASTAS/$1.fasta > $1_samples.ml.fasttree.opt.nwk
-unset OMP_NUM_THREADS
