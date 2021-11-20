e9256fa6bc4a18b1d4ee3ae83d60790f595bf6cd
Bryan Thornlow <bthornlo@ucsc.edu>
2021-11-20 13:50:33 -0500

    Delete getParsimony.sh

--- 3_incrementally_add_samples/3.2/getParsimony.sh
+++
@@ -1,5 +0,0 @@
-for i in {1..12}
-  do
-    usher -v /mnt/PARSIMONY_REAL_DATA/CUMULATIVE_VCFS/${i}_samples.vcf -t ${i}_samples.iqtree2.denovo.treefile -o ${i}_samples.iqtree2.denovo.pb
-    matUtils summary -i ${i}_samples.iqtree2.denovo.pb
-done;
