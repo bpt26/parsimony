d603597dd104e3fff7d6b4c8ff9ef01ab4eedbb0
Bryan Thornlow <bthornlo@ucsc.edu>
2021-11-20 14:11:17 -0500

    Update makeTrees.sh

--- 3_incrementally_add_samples/3.4/makeTrees.sh
+++ 3_incrementally_add_samples/3.4/makeTrees.sh
@@ -1,8 +1,6 @@
 export OMP_NUM_THREADS=15
 for i in {1..50}
   do
-    /usr/bin/time -o ${i}.ft.time -f "%E %M" FastTreeMP -nt -sprlength 1000 -nni 0 -spr 2 -log ${i}.fasttree.log -nosupport ${i}.fasta > ${i}_samples.ml.fasttree.opt.nwk
-    rm *ckp*
-    iqtree2 -s ${i}.fasta -te ${i}_samples.ml.fasttree.opt.nwk --epsilon 1.0 -m JC --no-opt-gamma-inv -blmin 0.00000000001 -nt 15 -pre JC | grep "BEST SCORE" > ${i}_samples.ml.fasttree.opt.likelihood.txt
+    /usr/bin/time -o ${i}.ft.gtrg.time -f "%E %M" FastTreeMP -gtr -gamma -nt -sprlength 1000 -nni 0 -spr 2 -log ${i}.fasttree.gtrg.log -nosupport /mnt/AGGREGATE_FASTAS/${i}.fasta > ${i}_samples.ml.fasttree.gtrg.opt.nwk
 done;
 unset OMP_NUM_THREADS
