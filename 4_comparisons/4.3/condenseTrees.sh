c9e0e50a878b93975b2a882275df3b0b993f9f73
Bryan Thornlow <bthornlo@ucsc.edu>
2021-11-22 16:46:58 -0500

    Create condenseTrees.sh

---
+++ 4_comparisons/4.3/condenseTrees.sh
@@ -0,0 +1,5 @@
+for i in {1..24}
+  do
+    usher -v ../CUMULATIVE_VCFS/${i}.vcf -t ${i}_samples.ml.fasttree.gtrg.opt.nwk -c -o ${i}_samples.ml.fasttree.gtrg.opt.condensed.pb -T 1
+    matUtils extract -i ${i}_samples.ml.fasttree.gtrg.opt.condensed.pb -t ${i}_samples.ml.fasttree.gtrg.opt.condensed.nwk -T 1
+done;
