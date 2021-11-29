7a55051e66ce3bc5359939741c8fbd37daf4978b
Bryan Thornlow <bthornlo@ucsc.edu>
2021-11-29 00:10:47 -0500

    Update condenseTrees.sh

--- 4_comparisons/4.1/condenseTrees.sh
+++ 4_comparisons/4.1/condenseTrees.sh
@@ -1,50 +1,6 @@
-usher -i 1_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/1.vcf -c -o 1_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 1_samples.ml.iqtree.opt.condensed.pb -t 1_samples.ml.iqtree.opt.condensed.nwk
-usher -i 2_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/2.vcf -c -o 2_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 2_samples.ml.iqtree.opt.condensed.pb -t 2_samples.ml.iqtree.opt.condensed.nwk
-usher -i 3_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/3.vcf -c -o 3_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 3_samples.ml.iqtree.opt.condensed.pb -t 3_samples.ml.iqtree.opt.condensed.nwk
-usher -i 4_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/4.vcf -c -o 4_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 4_samples.ml.iqtree.opt.condensed.pb -t 4_samples.ml.iqtree.opt.condensed.nwk
-usher -i 5_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/5.vcf -c -o 5_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 5_samples.ml.iqtree.opt.condensed.pb -t 5_samples.ml.iqtree.opt.condensed.nwk
-usher -i 6_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/6.vcf -c -o 6_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 6_samples.ml.iqtree.opt.condensed.pb -t 6_samples.ml.iqtree.opt.condensed.nwk
-usher -i 7_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/7.vcf -c -o 7_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 7_samples.ml.iqtree.opt.condensed.pb -t 7_samples.ml.iqtree.opt.condensed.nwk
-usher -i 8_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/8.vcf -c -o 8_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 8_samples.ml.iqtree.opt.condensed.pb -t 8_samples.ml.iqtree.opt.condensed.nwk
-usher -i 9_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/9.vcf -c -o 9_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 9_samples.ml.iqtree.opt.condensed.pb -t 9_samples.ml.iqtree.opt.condensed.nwk
-usher -i 10_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/10.vcf -c -o 10_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 10_samples.ml.iqtree.opt.condensed.pb -t 10_samples.ml.iqtree.opt.condensed.nwk
-usher -i 11_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/11.vcf -c -o 11_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 11_samples.ml.iqtree.opt.condensed.pb -t 11_samples.ml.iqtree.opt.condensed.nwk
-usher -i 12_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/12.vcf -c -o 12_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 12_samples.ml.iqtree.opt.condensed.pb -t 12_samples.ml.iqtree.opt.condensed.nwk
-usher -i 13_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/13.vcf -c -o 13_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 13_samples.ml.iqtree.opt.condensed.pb -t 13_samples.ml.iqtree.opt.condensed.nwk
-usher -i 14_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/14.vcf -c -o 14_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 14_samples.ml.iqtree.opt.condensed.pb -t 14_samples.ml.iqtree.opt.condensed.nwk
-usher -i 15_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/15.vcf -c -o 15_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 15_samples.ml.iqtree.opt.condensed.pb -t 15_samples.ml.iqtree.opt.condensed.nwk
-usher -i 16_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/16.vcf -c -o 16_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 16_samples.ml.iqtree.opt.condensed.pb -t 16_samples.ml.iqtree.opt.condensed.nwk
-usher -i 17_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/17.vcf -c -o 17_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 17_samples.ml.iqtree.opt.condensed.pb -t 17_samples.ml.iqtree.opt.condensed.nwk
-usher -i 18_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/18.vcf -c -o 18_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 18_samples.ml.iqtree.opt.condensed.pb -t 18_samples.ml.iqtree.opt.condensed.nwk
-usher -i 19_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/19.vcf -c -o 19_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 19_samples.ml.iqtree.opt.condensed.pb -t 19_samples.ml.iqtree.opt.condensed.nwk
-usher -i 20_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/20.vcf -c -o 20_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 20_samples.ml.iqtree.opt.condensed.pb -t 20_samples.ml.iqtree.opt.condensed.nwk
-usher -i 21_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/21.vcf -c -o 21_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 21_samples.ml.iqtree.opt.condensed.pb -t 21_samples.ml.iqtree.opt.condensed.nwk
-usher -i 22_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/22.vcf -c -o 22_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 22_samples.ml.iqtree.opt.condensed.pb -t 22_samples.ml.iqtree.opt.condensed.nwk
-usher -i 23_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/23.vcf -c -o 23_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 23_samples.ml.iqtree.opt.condensed.pb -t 23_samples.ml.iqtree.opt.condensed.nwk
-usher -i 24_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/24.vcf -c -o 24_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 24_samples.ml.iqtree.opt.condensed.pb -t 24_samples.ml.iqtree.opt.condensed.nwk
-usher -i 25_samples.ml.iqtree.opt.pb -v ../SIM_CUMULATIVE_VCFS/25.vcf -c -o 25_samples.ml.iqtree.opt.condensed.pb
-matUtils extract -i 25_samples.ml.iqtree.opt.condensed.pb -t 25_samples.ml.iqtree.opt.condensed.nwk
+for i in {1..25}
+  do
+    usher -v ../SIM_CUMULATIVE_VCFS/${i}.vcf -t ${i}_samples.ml.iqtree.opt.nwk -c -o ${i}_samples.ml.iqtree.opt.pb
+    matUtils extract -i ${i}_samples.ml.iqtree.opt.pb -R -O -o ${i}_samples.ml.iqtree.opt.COLLAPSED.pb -T 15
+    matUtils extract -i ${i}_samples.ml.iqtree.opt.COLLAPSED.pb -t ${i}_samples.ml.iqtree.opt.COLLAPSED.nwk -T 15
+done;
