274f4f1ba41666ca80f686f5d8f7fb3f8d103446
Bryan Thornlow <bthornlo@ucsc.edu>
2021-11-28 22:40:02 -0500

    Update condenseTrees.sh

--- 4_comparisons/4.4/condenseTrees.sh
+++ 4_comparisons/4.4/condenseTrees.sh
@@ -1,5 +1,6 @@
-for i in {1..26}
+for i in {1..25}
   do
-    usher -v ../CUMULATIVE_VCFS/${i}.vcf -t ${i}_samples.ml.fasttree.gtrg.opt.nwk -c -o ${i}_samples.ml.fasttree.gtrg.opt.condensed.pb -T 1
-    matUtils extract -i ${i}_samples.ml.fasttree.gtrg.opt.condensed.pb -t ${i}_samples.ml.fasttree.gtrg.opt.condensed.nwk -T 1
+    usher -v ../CUMULATIVE_VCFS/${i}.vcf -t ${i}_samples.ml.fasttree.gtrg.opt.nwk -c -o ${i}_samples.ml.fasttree.gtrg.opt.pb
+    matUtils extract -i ${i}_samples.ml.fasttree.gtrg.opt.pb -R -O -o ${i}_samples.ml.fasttree.gtrg.opt.COLLAPSED.pb -T 15
+    matUtils extract -i ${i}_samples.ml.fasttree.gtrg.opt.COLLAPSED.pb -t ${i}_samples.ml.fasttree.gtrg.opt.COLLAPSED.nwk -T 15
 done;
