ffd33682922c90c349ad4cd1cae0c062eeb0bd4a
Alex Kramer <alex.kramer@ucsc.edu>
2022-03-01 14:42:56 -0800

    Update makeTrees.sh

--- 4_comparisons/4.2b/makeTrees.sh
+++ 4_comparisons/4.2b/makeTrees.sh
@@ -1,1 +1,5 @@
-
+for i in {1..10}
+  do
+    rm *ckp*
+    /usr/bin/time -o ${i}.denovo.iq.time -f "%E %M" iqtree2 -s /mnt/SIM_USHER/IQ/${i}.fasta --suppress-list-of-sequences -nt 15 -blmin .000000000001 -pre ${i}_samples.iqtree2.denovo
+done;
