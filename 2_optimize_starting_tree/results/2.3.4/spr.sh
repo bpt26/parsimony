a557b93675207135e5e53a6aa160fb785ed24327
Alex Kramer <almkrame@ucsc.edu>
2022-04-25 23:04:33 +0000

    cleanup

--- 2_optimize_starting_tree/results/2.3.4/spr.sh
+++
@@ -1,6 +0,0 @@
-for i in {1..200}; do
-	if [[ $(( $i % 10 )) == 0 ]]; then
-		echo $i
-		./iqtree2-mpi -n 0 -no-ml -m JC -t resolve.nwk -s aln.fa -parsimony-spr 100 -parsimony-nni 100 -spr-radius $i --suppress-list-of-sequences -blfix -nt 15 -fast -pre spr$i
-	fi
-done
