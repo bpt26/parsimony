1247d5ffc627e56015f4990309f7e245d9eb3045
Alex Kramer <alex.kramer@ucsc.edu>
2022-05-01 12:02:57 -0700

    Update condenseTrees.sh

--- 4_simulated_data_experiments/scripts/4.7/condenseTrees.sh
+++ 4_simulated_data_experiments/scripts/4.7/condenseTrees.sh
@@ -1,6 +1,6 @@
 for i in {1..3}
   do
     usher -v ../../input/CUMULATIVE_VCFS/${i}.vcf -t "TREES/iteration_${i}/iter${i}.iq.stoch.denovo.treefile" -c -o "TREES/iteration_${i}/iter${i}.iq.stoch.denovo.pb"
-    matUtils extract -i "TREES/iteration_${i}/iter${i}.iq.stoch.denovo.pb" -R -O -o "TREES/iteration_${i}/iter${i}.iq.stoch.denovo.COLLAPSED.pb" -T 15
+    matUtils extract -i "TREES/iteration_${i}/iter${i}.iq.stoch.denovo.pb" -O -o "TREES/iteration_${i}/iter${i}.iq.stoch.denovo.COLLAPSED.pb" -T 15
     matUtils extract -i "TREES/iteration_${i}/iter${i}.iq.stoch.denovo.COLLAPSED.pb" -t "TREES/iteration_${i}/iter${i}.iq.stoch.denovo.COLLAPSED.nwk" -T 15
 done;
